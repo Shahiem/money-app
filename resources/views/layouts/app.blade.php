e6c66c5ad62593c92a13ad9fea1cd5c60cf37fdf
Shahiem <s.seymor@live.nl>
2018-03-07 14:46:19 +0100

    A few assets changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,24 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
     </head>
     <body>
+        <nav class="uk-navbar-container" uk-navbar>
+            <div class="uk-navbar-left">
+                <ul class="uk-navbar-nav">
+                    <li class="uk-active"><a href="#">Active</a></li>
+                    <li>
+                        <a href="#">Parent</a>
+                    </li>
+                    <li><a href="#">Source</a></li>
+                </ul>
+            </div>
+            <div class="uk-navbar-left">
+                <ul class="uk-navbar-nav">
+                    <li class="uk-active"><a href="#">Sign Up</a></li>
+                    <li class="uk-active"><a href="#">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+
         <div class="container">
             @yield('content')
         </div>
